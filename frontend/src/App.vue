1818e1f053f27d7664991848d240d1c7be50b045
Sophie Santana <ssophiesantana@gmail.com>
2024-02-11 21:39:55 -0300

    feat: inicia frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <main>
+    <Header />
+    <nav id="links-header">
+      <RouterLink to="/login">Login Adm</RouterLink>
+      <RouterLink to="/sign-in-adm">Cadastro de Adm</RouterLink>
+      <RouterLink to="/sign-in-costumer">Cadastro de Cliente</RouterLink>
+    </nav>
+  <RouterView />
+  </main>
+</template>
+
+<style scoped>
+
+#links-header {
+  background-color: yellow;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: green;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid black;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <form action="" id="form">
+    <div id="container">
+      <div>
+        <label for="email">CPF:</label>
+        <input type="email" name="email">
+      </div>
+
+      <div>
+        <label for="password">Senha:</label>
+        <input type="password" name="password">
+      </div>
+
+      <div id="btn-recover-pass">
+        <button type="submit">Logar</button>
+        <p>Esqueceu a senha?</p>
+      </div>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+
+#form {
+  background-color: blueviolet;
+  text-align: center;
+}
+
+#container {
+  display: flex;
+  flex-direction: column;
+}
+
+#btn-recover-pass {
+  display: flex;
+  justify-content: center;
+  background-color: aqua;
+}
+
+</style>
