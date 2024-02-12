a473c9b3876c6f6185816a2443c9fd84987cb693
Sophie Santana <ssophiesantana@gmail.com>
2024-02-12 20:15:16 -0300

    feat/frontend: implementa logica de login para enviar os dados do front para o back

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,14 +1,51 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      formData: {
+        cpf: '',
+        password: '',
+        url: window.location.href
+      },
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      this.isLoading = true;
+      this.error = null;
+
+      axios.post('http://localhost/backend/', this.formData, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(response => {
+          console.log(response.data);
+          if (response.data === 'User Authenticated!') {
+            this.$router.push('/costumers');
+          }
+        }).catch(error => {
+          console.error('Error sending data', error);
+        });
+    },
+  },
+};
+</script>
+
 <template>
-  <form action="" id="form">
+  <form id="form" @submit.prevent="handleSubmit">
     <div id="container">
       <div>
-        <label for="email">CPF:</label>
-        <input type="email" name="email">
+        <label for="cpf">CPF:</label>
+        <input type="text" name="cpf" v-model="formData.cpf">
       </div>
 
       <div>
         <label for="password">Senha:</label>
-        <input type="password" name="password">
+        <input type="password" name="password" v-model="formData.password">
       </div>
 
       <div id="btn-recover-pass">
